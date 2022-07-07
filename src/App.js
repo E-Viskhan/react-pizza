45878486cc7f16ca8cb6ac7ea409bdee718ccf43
E-Viskhan <viskhan11@gmail.com>
2022-07-07 22:10:09 +0300

    Added getting pizzas from server

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pizzas, setPizzas] = useState([]);
+
+  useEffect(() => {
+    fetch('https://62c70b8074e1381c0a70239f.mockapi.io/pizzas')
+      .then((res) => res.json())
+      .then((data) => setPizzas(data));
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
