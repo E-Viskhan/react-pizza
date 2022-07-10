61e46061f194038f460a6c1684b0e83e2d32f3b9
E-Viskhan <viskhan11@gmail.com>
2022-07-10 19:54:56 +0300

    added ui for search

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,19 +1,23 @@
 import logoSvg from '../assets/img/pizza-logo.svg';
 import { Link } from 'react-router-dom';
+import Search from './Search';
 
 const Header = () => {
   return (
     <div className="header">
       <div className="container">
-        <Link to="/">
-          <div className="header__logo">
-            <img width="38" src={logoSvg} alt="Pizza logo" />
-            <div>
-              <h1>React Pizza</h1>
-              <p>самая вкусная пицца во вселенной</p>
+        <div className="header__wrapper">
+          <Link to="/" className="header__link">
+            <div className="header__logo">
+              <img width="38" src={logoSvg} alt="Pizza logo" />
+              <div>
+                <h1>React Pizza</h1>
+                <p>самая вкусная пицца во вселенной</p>
+              </div>
             </div>
-          </div>
-        </Link>
+          </Link>
+          <Search />
+        </div>
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
