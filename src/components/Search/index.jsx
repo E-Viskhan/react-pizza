cf800d02720578aea9db0357ff9fbad8345f76f3
E-Viskhan <viskhan11@gmail.com>
2022-07-15 04:00:05 +0300

    Added debounce search queries

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,12 +1,33 @@
 import styles from './Search.module.scss';
 import searchIcon from '../../assets/img/search.svg';
 import clearIcon from '../../assets/img/close.svg';
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 import { setSearchValue } from '../../redux/slices/filterSlice';
+import { useCallback, useRef, useState } from 'react';
+import { debounce } from 'lodash';
 
 const Search = (props) => {
-  const searchValue = useSelector((state) => state.filter.searchValue);
+  const [value, setValue] = useState('');
   const dispatch = useDispatch();
+  const inputEl = useRef(null);
+
+  const onClickClear = () => {
+    dispatch(setSearchValue(''));
+    setValue('');
+    inputEl.current.focus();
+  };
+
+  const onChangeInput = (e) => {
+    updateSearchValue(e.target.value);
+    setValue(e.target.value);
+  };
+
+  const updateSearchValue = useCallback(
+    debounce((str) => {
+      dispatch(setSearchValue(str));
+    }, 500),
+    []
+  );
 
   return (
     <div className={styles.wrapper}>
@@ -16,16 +37,17 @@ const Search = (props) => {
         alt=""
       />
       <input
+        ref={inputEl}
         className={styles.input}
         placeholder="Поиск пиццы..."
-        value={searchValue}
-        onChange={(e) => dispatch(setSearchValue(e.target.value))}
+        value={value}
+        onChange={onChangeInput}
       />
-      {searchValue && (
+      {value && (
         <img
           src={clearIcon}
           className={`${styles.icon} ${styles.clearIcon}`}
-          onClick={() => dispatch(setSearchValue(''))}
+          onClick={onClickClear}
           alt="Кнопка очистки поля поиска"
         />
       )}
